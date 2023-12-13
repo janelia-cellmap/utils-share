c10ba2b3cb280c8a5071b5f6b5adf58ebef74e5e
rhoadesScholar <rhoadesj@hhmi.org>
2023-12-13 11:26:34 -0500

    making blockwise segment cluster friendly

--- zouinkhim/postprocessing/daisy/segment_worker.py
+++ zouinkhim/postprocessing/daisy/segment_worker.py
@@ -1,66 +1,110 @@
+import os
+import sys
+from config import *
 import daisy
+from funlib.persistence import Array
+
 import logging
-import time
-
-
-import skimage.measure
-import skimage.filters
-import skimage.morphology
-from skimage.segmentation import watershed
-from skimage.morphology import dilation, cube, ball
-from scipy import ndimage as ndi
-import daisy
 import numpy as np
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-
-
-def segment_process(
-    array_in, 
-	roi,
-    threshold=0.5,
-    gaussian_kernel=2
-):
-    mito_dist = array_in.to_ndarray(roi, fill_value=0)
-    mito_dist = skimage.filters.gaussian(mito_dist, sigma=gaussian_kernel)
-    print("done gaussian", mito_dist.shape, mito_dist.max(), mito_dist.min())
-    binary_peroxi = mito_dist > threshold
-    print("done threshold", binary_peroxi.shape)
-    markers, _ = ndi.label(binary_peroxi)
-    # Apply Watershed
-    ws_labels = watershed(-mito_dist, markers, mask=binary_peroxi)
-    print("done watershed", ws_labels.shape)
-    instance_peroxi = skimage.measure.label(ws_labels).astype(np.int64)
-    print("done instance", instance_peroxi.shape, instance_peroxi.max(), instance_peroxi.min())
-    instance_peroxi[binary_peroxi == 0] = 0
-    print("done mask instance", instance_peroxi.shape)
-    instance_peroxi = dilation(instance_peroxi, cube(2))
-    #  dilation(LD[:], cube(2))
-
-    return instance_peroxi.astype(np.uint64)
-
-
-def segment_worker():
-
+def segment_worker(tmpdir):
+    num_voxels_in_block = block_size.size
     client = daisy.Client()
 
     while True:
         logger.info("getting block")
         with client.acquire_block() as block:
-
             if block is None:
                 break
 
-            logger.info(f"got block {block}")
+            logger.debug("Segmenting in block %s", block)
 
-            
-            
+            segmentation = segment_function(block.read_roi)
+
+            print("========= block %d ====== " % block.block_id[1])
+            print(segmentation)
+
+            assert segmentation.dtype == np.uint64
+
+            id_bump = block.block_id[1] * num_voxels_in_block
+            segmentation += id_bump
+            segmentation[segmentation == id_bump] = 0
+
+            logger.debug("Bumping segmentation IDs by %d", id_bump)
+
+            # wrap segmentation into daisy array
+            segmentation = Array(
+                segmentation, roi=block.read_roi, voxel_size=array_in.voxel_size
+            )
+
+            # store segmentation in out array
+            array_out[block.write_roi] = segmentation[block.write_roi]
+
+            neighbor_roi = block.write_roi.grow(
+                array_in.voxel_size, array_in.voxel_size
+            )
+
+            # clip segmentation to 1-voxel context
+            segmentation = segmentation.to_ndarray(roi=neighbor_roi, fill_value=0)
+            neighbors = array_out.to_ndarray(roi=neighbor_roi, fill_value=0)
+
+            unique_pairs = []
+
+            for d in range(3):
+                slices_neg = tuple(
+                    slice(None) if dd != d else slice(0, 1) for dd in range(3)
+                )
+                slices_pos = tuple(
+                    slice(None) if dd != d else slice(-1, None) for dd in range(3)
+                )
+
+                pairs_neg = np.array(
+                    [
+                        segmentation[slices_neg].flatten(),
+                        neighbors[slices_neg].flatten(),
+                    ]
+                )
+                pairs_neg = pairs_neg.transpose()
+
+                pairs_pos = np.array(
+                    [
+                        segmentation[slices_pos].flatten(),
+                        neighbors[slices_pos].flatten(),
+                    ]
+                )
+                pairs_pos = pairs_pos.transpose()
+
+                unique_pairs.append(
+                    np.unique(np.concatenate([pairs_neg, pairs_pos]), axis=0)
+                )
+
+            unique_pairs = np.concatenate(unique_pairs)
+            zero_u = unique_pairs[:, 0] == 0
+            zero_v = unique_pairs[:, 1] == 0
+            non_zero_filter = np.logical_not(np.logical_or(zero_u, zero_v))
+
+            logger.debug("Matching pairs with neighbors: %s", unique_pairs)
+
+            edges = unique_pairs[non_zero_filter]
+            nodes = np.unique(edges)
+
+            logger.debug("Final edges: %s", edges)
+            logger.debug("Final nodes: %s", nodes)
+
+            np.savez_compressed(
+                os.path.join(tmpdir, "block_%d.npz" % block.block_id[1]),
+                nodes=nodes,
+                edges=edges,
+            )
 
             logger.info(f"releasing block: {block}")
 
 
 if __name__ == "__main__":
-
-    segment_worker()
+    # get tmpdir from command line arguments
+    args = sys.argv[1:]
+    segment_worker(args[0])
